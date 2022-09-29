dcc014770bc5843c48a36c96193cd7b970dfd8a5
Artur <109510338+ArturNersesian@users.noreply.github.com>
2022-09-29 20:44:08 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+
+
+TOKEN = '5528814020:AAG8KwKBD0Ab9CcejXyW2cGVlp6B9EEtQoY'
+keys = {'евро': 'EUR',
+        'доллар': 'USD',
+        'рубль': 'RUB',
+        'bitcoin': 'BTC',
+        'ethereum': 'ETH',
+        'tether': 'USDT',
+        'solana': 'SOL',
+        'binance_coin':'BNB',
+        'cosmos':'ATOM',
+        'busd':'BUSD'}
